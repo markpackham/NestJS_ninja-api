f3c46754b6241dec86b9b54b24b9008a4fb2abbb
mark <markpackham1@gmail.com>
2023-02-09 22:25:33 +0000

    demo get and show
    https://youtu.be/QTA8emDmH-s?t=202

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+
+// GET /ninjas -> return an empty array
+    @Get()
+    getNinjas(){
+        return [];
+    }
+
+
+    @Get(':id')
+    getOneNinja(){
+        return [];
+    }
+}
+
