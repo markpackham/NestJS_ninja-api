22024e511507abc24118dc825bc2724e989e577c
mark <markpackham1@gmail.com>
2023-02-09 22:49:38 +0000

    update the Put for ninja updates
    https://youtu.be/QTA8emDmH-s?t=578

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Post, Put, Delete, Param, Query, Body } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
 export class NinjasController {
@@ -31,9 +32,10 @@ export class NinjasController {
 
 
     @Put(':id')
-    updateNinja(@Param('id') id: string){
+    updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto){
         return {
             id,
+            name: updateNinjaDto.name,
         };
     }
 
