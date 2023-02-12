5bb2632e15a552dff828faabb9c24d52ea943776
mark <markpackham1@gmail.com>
2023-02-12 23:02:11 +0000

    add model like business logic via ninja Provider
    https://youtu.be/MPYNkau4Bgg?t=90

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
+// The Providers that live in services are for business logic (like the Model in MVC)
 @Injectable()
-export class NinjasService {}
+export class NinjasService {
+    private ninjas = [
+        {id: 0, name: 'ninjaA', weapon: 'stars'},
+        {id: 1, name: 'ninjaB', weapon: 'nunchucks'},
+    ];
+
+    getNinjas(weapon?: 'stars' | 'nunchucks') {
+        if(weapon){
+            return this.ninjas.filter((ninja) => ninja.weapon === weapon);
+        }
+        return this.ninjas;
+    }
+}
