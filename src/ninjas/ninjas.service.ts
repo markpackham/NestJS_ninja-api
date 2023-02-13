6bf6c2d4b3edeae9aad7c4b6868e68e9398fc306
mark <markpackham1@gmail.com>
2023-02-13 10:04:35 +0000

    create new ninja service
    https://youtu.be/MPYNkau4Bgg?t=426

--- src/ninjas/ninjas.service.ts
+++ src/ninjas/ninjas.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
 
 // The Providers that live in services are for business logic (like the Model in MVC)
+// the injectable is used for dependency injection so we can ram functions into functions aka high order functions
 @Injectable()
 export class NinjasService {
     private ninjas = [
@@ -13,5 +15,28 @@ export class NinjasService {
             return this.ninjas.filter((ninja) => ninja.weapon === weapon);
         }
         return this.ninjas;
+    }
+
+
+    getNinja(id: number){
+        const ninja = this.ninjas.find((ninja) => ninja.id === id);
+
+        if(!ninja){
+            throw new Error('ninja not found');
+        }
+
+        return ninja;
+    }
+
+    createNinja(createNinjaDto: CreateNinjaDto){
+
+        const newNinja = {
+            ...createNinjaDto,
+            id: Date.now(),
+        }
+
+        this.ninjas.push(newNinja);
+
+        return newNinja;
     }
 }
