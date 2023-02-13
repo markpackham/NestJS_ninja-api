2401859b87f20e642e6b37acabb5481cdfb731f6
mark <markpackham1@gmail.com>
2023-02-13 09:32:54 +0000

    create a constructor to avoid having to create many class instances
    https://youtu.be/MPYNkau4Bgg?t=309

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -6,13 +6,23 @@ import { NinjasService } from './ninjas.service';
 @Controller('ninjas')
 export class NinjasController {
 
+    // Nest instantiates the classes for us
+    // behind the scenes it's doing something like this,
+    // const controller = new NinjasController();
+    constructor(private readonly ninjasService: NinjasService){}
+
 // GET /ninjas -> return ninjas
 // GET /ninjas?weapon=stars -> return a stars ninja
 // eg http://localhost:3000/ninjas?weapon=stars
     @Get()
     getNinjas(@Query('weapon') weapon: 'stars' | 'nunchucks'){
-        const service = new NinjasService();
-        return service.getNinjas(weapon);
+        // use the contructor instead of having to create a new instance every single time
+        // Nest does the following for us so we don't need to bother once we have a constructor
+        //const service = new NinjasService();
+
+        // No longer needs to be done, we can us "this"
+        //return service.getNinjas(weapon);
+        return this.ninjasService.getNinjas(weapon);
     }
 
 
