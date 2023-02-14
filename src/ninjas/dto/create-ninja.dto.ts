e86e230884288d303712e41085a93dc14c645658
mark <markpackham1@gmail.com>
2023-02-14 12:50:51 +0000

    use @IsEnum to force selection of only valid weapons for ninja creation
    https://youtu.be/dxPZzkXX9GE?t=352

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,8 +1,9 @@
-import { MinLength } from "class-validator";
+import { MinLength, IsEnum } from "class-validator";
 
 export class CreateNinjaDto {
     @MinLength(3)
     name: string;
 
+    @IsEnum(['stars', 'nunchucks'], {message: 'You can only chose stars OR nunchucks!'})
     weapon: 'stars' | 'nunchucks';
 }
